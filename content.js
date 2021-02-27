dc6a2e94980a8c14dce98176bc8969993b8095ca
kdty <63476491+kdty@users.noreply.github.com>
2021-02-27 17:07:26 +0900

    Update content.js
    見直し

--- content.js
+++ content.js
@@ -114,6 +114,11 @@ var price50plus = 0;
 var price40plus = 0;
 var price30plus = 0;
 
+var FETCH_STAT = {	NOT_YET: 1,
+					FETTING:2,
+					DONE: 3
+};
+
 function pointToColor(points){
 	var point = points.match(/.+\((\d+)%\)/);
 	if(point[1]){
@@ -158,22 +163,17 @@ async function wishpoints(enablefetch){
 	const dom_parser = new DOMParser();
 	//wishlist内のアイテムのリスト
 	const itemList = document.getElementsByClassName("price-section");
-	//どこまで読み込んだのかsessionStorageに記録
-	const olditemnum = sessionStorage.getItem("storageItemNum")||0;
-	//debug
-	//console.log(itemList);
-	console.log("olditemnum: " + olditemnum);
-	
-	const totalProcessedItemsCnt = olditemnum + itemList.length;
-	var processedItemsCnt = olditemnum;
-	
+
+	const totalItemsCnt = itemList.length;
+	var processedItemsCnt = 0;
+		
 	var bbb = document.getElementById("processedPercent");
 	
 	if(bbb == null)
 	{
 		var aaa = document.getElementById("listPrivacy");
 		console.log(aaa);
-		aaa.insertAdjacentHTML("afterend", "<br><span id=\"processedPercent\">" + processedItemsCnt / totalProcessedItemsCnt * 100 + "%</span>" + 
+		aaa.insertAdjacentHTML("afterend", "<br><span id=\"processedPercent\">" + processedItemsCnt / totalItemsCnt * 100 + "%</span>" + 
 											"<br><span id=\"point50plus\">over 50%: " + point50plus + "</span>" +
 											"<br><span id=\"point40plus\">40%～49%: " + point40plus + "</span>" +
 											"<br><span id=\"point30plus\">30%～39%: " + point30plus + "</span>"
@@ -186,76 +186,95 @@ async function wishpoints(enablefetch){
 	var ddd = document.getElementById("point40plus");
 	var eee = document.getElementById("point30plus");
 	
+	
+	
 	//以前に調べてないアイテムに対しfetchを行う
-	for(let item of Array.from(itemList).slice(olditemnum)){
+	for(let item of Array.from(itemList)){
 		const asin = JSON.parse(item.attributes["data-item-prime-info"].value).asin;
 		console.log("asin: " + asin);
-		
-		if(enablefetch){
-			console.log("fetch:" + asin);
-			fetch('https://www.amazon.co.jp/dp/'+asin,{credentials: 'omit', referrer: '', referrerPolicy: 'no-referrer'})
-			.then(res=>res.text())
-			.then(text=>{
-				const lopoints = dom_parser.parseFromString(text, "text/html").getElementsByClassName("loyalty-points");
-				//debug
-				//console.log(lopoints);
-				//loyalty-pointsがない場合にはエラーが出るため存在判定
-				let points = "";
-				//console.log(lopoints.length);
-				if(lopoints.length!=0){
-					//points = lopoints[0].children[1].innerText.trim();
-					points = lopoints[0].innerText.trim().replace(/\s+/g, "").replace(/獲得ポイント:/g,"");
+		const key = "asin:" + asin;
+		if(sessionStorage.getItem(key) === null)
+		{
+			sessionStorage.setItem(key, FETCH_STAT.NOT_YET);
+			if(enablefetch){
+				console.log("fetch:" + asin);
+				fetch('https://www.amazon.co.jp/dp/'+asin,{credentials: 'omit', referrer: '', referrerPolicy: 'no-referrer'})
+				.then(res=>res.text())
+				.then(text=>{
+					
+					const lopoints = dom_parser.parseFromString(text, "text/html").getElementsByClassName("loyalty-points");
+					//debug
+					//console.log(lopoints);
+					//loyalty-pointsがない場合にはエラーが出るため存在判定
+					let points = "";
+					//console.log(lopoints.length);
+					if(lopoints.length!=0){
+						//points = lopoints[0].children[1].innerText.trim();
+						points = lopoints[0].innerText.trim().replace(/\s+/g, "").replace(/獲得ポイント:/g,"");
+						//console.log(points);
+						var spanColor = pointToColor(points);
+						//console.log(spanColor);
+						var bgColor="#FFFFFF";
+						if(spanColor!="#000000"){
+							bgColor="#CCCCCC";
+						}
+						var pAsin = document.getElementById("points_" + asin);
+						if (pAsin == null)
+						{
+							var insertText = '<br><span id=points_' + asin + " class=\"a-price\" data-a-size=\"m\" style=\"background-color:" + bgColor + "; color:" + spanColor + ";\">" + points + "</span>";
+							console.log(insertText);
+							item.firstElementChild.insertAdjacentHTML("afterend", insertText);
+							
+							processedItemsCnt++;
+							sessionStorage.setItem(key, FETCH_STAT.DONE);
+						}
+					}
+					//debug
 					//console.log(points);
-					var spanColor = pointToColor(points);
-					//console.log(spanColor);
-					var bgColor="#FFFFFF";
-					if(spanColor!="#000000"){
-						bgColor="#CCCCCC";
-					}
-					var pAsin = document.getElementById("points_" + asin);
-					if (pAsin == null)
-					{
-						var insertText = '<br><span id=points_' + asin + " class=\"a-price\" data-a-size=\"m\" style=\"background-color:" + bgColor + "; color:" + spanColor + ";\">" + points + "</span>";
-						console.log(insertText);
-						item.firstElementChild.insertAdjacentHTML("afterend", insertText);
-					}
-				}
+					
+					//console.log("processedItemsCnt: " + processedItemsCnt);
+					//bbb.textContent = processedItemsCnt / totalItemsCnt * 100 + "%";
+					bbb.textContent = processedItemsCnt + "/" + totalItemsCnt;
+					ccc.textContent = "over 50%: " + point50plus;
+						ddd.textContent = "40%～49%: " + point40plus;
+						eee.textContent = "30%～39%: " + point30plus;
+				}).catch(err=>console.error(err));
+			}else{
 				//debug
-				//console.log(points);
-				processedItemsCnt++;
-				//console.log("processedItemsCnt: " + processedItemsCnt);
-				bbb.textContent = processedItemsCnt / totalProcessedItemsCnt * 100 + "%";
-				ccc.textContent = "over 50%: " + point50plus;
+				// console.log("jquery ajax");
+				$.ajax({
+					type:'GET',
+					url:'https://www.amazon.co.jp/dp/'+asin,
+					dataType:'html'
+				}).done(function(data,status,xhr){
+					const lopoints = dom_parser.parseFromString(data, "text/html").getElementsByClassName("loyalty-points");
+					//kindle本でポイントがついている場合のみ計算
+					if(lopoints.length){
+						//trimをすることでスペースを削除
+						var points = lopoints[0].children[1].innerText.trim();
+						//debug
+						// console.log(kindlepoints);
+						item.firstElementChild.insertAdjacentHTML("beforeend", "<br><span>" + points + "</span>");
+					}
+				}).fail(function(xhr,status,error){
+					console.error(error);
+				});
+			};
+		
+			await sleep(waitTime);
+		}
+		else if(sessionStorage.getItem(key) == FETCH_STAT.DONE)
+		{
+			processedItemsCnt++;
+			//console.log("processedItemsCnt: " + processedItemsCnt);
+			//bbb.textContent = processedItemsCnt / totalItemsCnt * 100 + "%";
+			bbb.textContent = processedItemsCnt + "/" + totalItemsCnt;
+			ccc.textContent = "over 50%: " + point50plus;
 				ddd.textContent = "40%～49%: " + point40plus;
 				eee.textContent = "30%～39%: " + point30plus;
-			}).catch(err=>console.error(err));
-		}else{
-			//debug
-			// console.log("jquery ajax");
-			$.ajax({
-				type:'GET',
-				url:'https://www.amazon.co.jp/dp/'+asin,
-				dataType:'html'
-			}).done(function(data,status,xhr){
-				const lopoints = dom_parser.parseFromString(data, "text/html").getElementsByClassName("loyalty-points");
-				//kindle本でポイントがついている場合のみ計算
-				if(lopoints.length){
-					//trimをすることでスペースを削除
-					var points = lopoints[0].children[1].innerText.trim();
-					//debug
-					// console.log(kindlepoints);
-					item.firstElementChild.insertAdjacentHTML("beforeend", "<br><span>" + points + "</span>");
-				}
-			}).fail(function(xhr,status,error){
-				console.error(error);
-			});
-		};
+		}
 		
-		await sleep(waitTime);
+		
 	}
-	//debug
-	console.log(itemList.length);
-	//セッションストレージに現在の読み込み数を記録
-	sessionStorage.setItem("storageItemNum", itemList.length);
 };
 
